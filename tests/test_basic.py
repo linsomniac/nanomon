a52bbad1b32f82a58758baf612b835d19f46319b
Sean Reifschneider <sean@realgo.com>
2015-04-21 15:58:52 -0600

    Adding test to demonstrate #4.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -39,14 +39,11 @@ class test_Basic(unittest.TestCase):
 			pass
 
 	def tearDown(self):
-		try:
-			os.remove('nanomon.status')
-		except OSError:
-			pass
-		try:
-			os.remove('test_checker')
-		except OSError:
-			pass
+		for filename in ['test_checker', 'status_1', 'status_2', 'nanomon.status']:
+			try:
+				os.remove(filename)
+			except OSError:
+				pass
 
 	def test_Basic(self):
 		#  make checker fail
@@ -95,6 +92,99 @@ class test_Basic(unittest.TestCase):
 				output='Subject: UP: Service restored: test_checker')
 		check_command(self, ['python', self.nanomon_path,
 				'--config', 'basic_config', 'status'],
+				output='OK', exit_code=0)
+
+
+	def test_SwapFailure(self):
+		#  make checker fail
+		with open('test_checker', 'w') as fp:
+			fp.write('#!/bin/bash\n\nexit `cat $1`\n')
+		os.chmod('test_checker', 0755)
+
+		with open('status_1', 'w') as fp:
+			fp.write('0')
+		with open('status_2', 'w') as fp:
+			fp.write('0')
+
+		check_command(self, ['python', self.nanomon_path,
+				'--config', 'swapfailure_config', 'reset'])
+
+		for i in xrange(10):
+			check_command(self, ['python', self.nanomon_path,
+					'--config', 'swapfailure_config'])
+			check_command(self, ['python', self.nanomon_path,
+					'--config', 'swapfailure_config', 'status'],
+					output='OK', exit_code=0)
+
+        #  make first one fail
+		with open('status_1', 'w') as fp:
+			fp.write('1')
+		with open('status_2', 'w') as fp:
+			fp.write('0')
+
+		for i in xrange(5):
+			check_command(self, ['python', self.nanomon_path,
+					'--config', 'swapfailure_config'])
+			check_command(self, ['python', self.nanomon_path,
+					'--config', 'swapfailure_config', 'status'],
+					output=', UP', exit_code=1)
+
+		#  Check e-mail subject
+		check_command(self, ['python', self.nanomon_path,
+				'--config', 'swapfailure_config'],
+				output='Subject: DOWN: Service OUTAGE: test_checker')
+		check_command(self, ['python', self.nanomon_path,
+				'--config', 'swapfailure_config', 'status'],
+				output=', DOWN', exit_code=1)
+
+        #  make only second one fail
+		with open('status_1', 'w') as fp:
+			fp.write('0')
+		with open('status_2', 'w') as fp:
+			fp.write('1')
+
+        #  should receive an up from the first test
+		check_command(self, ['python', self.nanomon_path,
+				'--config', 'swapfailure_config'],
+				output='Subject: UP: Service restored: test_checker')
+		check_command(self, ['python', self.nanomon_path,
+				'--config', 'swapfailure_config', 'status'],
+				output='OK', exit_code=0)
+
+		for i in xrange(5):
+			check_command(self, ['python', self.nanomon_path,
+					'--config', 'swapfailure_config'])
+			check_command(self, ['python', self.nanomon_path,
+					'--config', 'swapfailure_config', 'status'],
+					output=', UP', exit_code=1)
+
+        #  now should fail for the second one
+		check_command(self, ['python', self.nanomon_path,
+				'--config', 'swapfailure_config'],
+				output='Subject: DOWN: Service OUTAGE: test_checker')
+		check_command(self, ['python', self.nanomon_path,
+				'--config', 'swapfailure_config', 'status'],
+				output=', DOWN', exit_code=1)
+
+		for i in xrange(5):
+			check_command(self, ['python', self.nanomon_path,
+					'--config', 'swapfailure_config'])
+			check_command(self, ['python', self.nanomon_path,
+					'--config', 'swapfailure_config', 'status'],
+					output=', DOWN', exit_code=1)
+
+		#  make checker succeed
+		with open('status_1', 'w') as fp:
+			fp.write('0')
+		with open('status_2', 'w') as fp:
+			fp.write('0')
+
+		#  Check e-mail subject
+		check_command(self, ['python', self.nanomon_path,
+				'--config', 'swapfailure_config'],
+				output='Subject: UP: Service restored: test_checker')
+		check_command(self, ['python', self.nanomon_path,
+				'--config', 'swapfailure_config', 'status'],
 				output='OK', exit_code=0)
 
 
